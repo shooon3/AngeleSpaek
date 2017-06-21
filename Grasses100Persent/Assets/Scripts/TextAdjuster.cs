c24fe26297d119cf62ad60d7484d3cf6c8277bf2
DESKTOP-3KKKU4H\user <nkc.hiyori.saitou@gmail.com>
2017-06-21 13:28:09 +0900

    no message

---
+++ Grasses100Persent/Assets/Scripts/TextAdjuster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine; 
+using System.Collections; 
+ 
+[ExecuteInEditMode] 
+public class TextAdjuster : MonoBehaviour{ 
+ 
+    [Range(0, 10)] 
+    public float fontScale; 
+    TextMesh tetxMesh; 
+ 
+    void Start() {
+        tetxMesh = GetComponent<TextMesh>();
+    } 
+ 	 
+    void Update(){ 
+        Vector3 defaultScale = new Vector3(1, 1, 1) * fontScale; 
+        int fontSize = tetxMesh.fontSize; 
+        fontSize = fontSize == 0 ? 12 : fontSize; 
+ 
+        float scale = 0.1f * 128 / fontSize; 
+        transform.localScale = defaultScale* scale; 
+    } 
+} 
